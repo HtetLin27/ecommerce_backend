ab2140213366cd6d20cfdec90de8c6b7a54f420d
HtetLin27 <105052327+HtetLin27@users.noreply.github.com>
2022-07-11 22:17:06 +0630

    initial latest code commit

---
+++ index.js
@@ -0,0 +1,173 @@
+const bcrypt =require('bcrypt')
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+const fileUpload=require("express-fileupload");
+const {createDB,createColletion,insertProduct,getAllData,getData,updateData,deleteData,insertService,insertUser} = require("./models/DB")
+
+app.use(function(req,res,next){
+    res.setHeader("Access-Control-Allow-Origin","http://localhost:3000");
+    next();
+})
+app.get('/',function(req,res){
+    res.send("Hello")
+});
+app.post('/product',fileUpload(),function(req,res){
+   let body=  JSON.parse(req.body.data);
+   let title = body.title;
+   let des =body.des;
+   let price = body.price;
+   let random = new Date().getTime();
+   let imageName = random + req.files.image.name;
+  req.files.image.mv("./uploads/"+imageName);
+   try{
+    insertProduct(title,des,price,imageName,"one_click","product");
+    res.send({message:"Product Created Successfully",status:true})
+   }catch(e){
+    res.send({message:"Somthing was wrong",status:false})
+   }
+
+    res.send({message:"Hello",status:true});
+})
+app.get('/products',function(req,res){
+    try{
+        getAllData("one_click","product",res);
+      
+    }catch(e){
+
+    }
+})
+
+app.get('/db',function(req,res){
+    try{
+            createDB("one_click");
+            res.send("OK")
+    } catch(e){}
+    
+})
+app.get('/collection',function(req,res){
+   try{
+    createColletion("one_click","users");
+    res.send("Collection created Successfully")
+   }catch(e){
+
+   }
+})
+app.get('/products/:id',function(req,res){
+    try{
+    getData("one_click","product",req.params.id,res);
+    }catch(e){
+
+    }
+})
+app.post('/products/:id',fileUpload(),function(req,res){
+    let body = JSON.parse(req.body.data);
+    let title = body.title;
+    let des = body.des;
+    let price =body.price;
+    
+    let random = new Date().getTime();
+    let image = random+ "_"+req.files.image.name;
+    req.files.image.mv("./uploads"+image);
+
+    try{
+       let update ={
+        title:title,
+        description:des,
+        price:price,
+        image:image
+       }
+       updateData("one_click","product",req.params.id,update);
+       res.send({message:"Produt Updated Successfully",status:true});
+    }catch(e){
+        res.send({message:"Something Wrong",status:false});
+    }
+})
+app.post('/products/:id/delete',fileUpload(),function(req,res){
+    var id = req.params.id;
+    deleteData("one_click","product",id);
+    res.send({message:"Product Deleted Successfully",status:true})
+})
+app.post('/service',fileUpload(),function(req,res){
+    let body=  JSON.parse(req.body.data);
+    let title = body.title;
+    let des =body.des;
+  
+    let random = new Date().getTime();
+    let imageName = random + req.files.image.name;
+   req.files.image.mv("./uploads/"+imageName);
+    try{
+     insertService(title,des,imageName,"one_click","service");
+     res.send({message:"Service Created Successfully",status:true})
+    }catch(e){
+     res.send({message:"Something was wrong",status:false})
+    }
+ 
+     res.send({message:"Hello",status:true});
+ })
+ app.post('/services/:id',fileUpload(),function(req,res){
+    let body = JSON.parse(req.body.data);
+    let title = body.title;
+    let des = body.des;
+    
+    let random = new Date().getTime();
+    let image = random+ "_"+req.files.image.name;
+    req.files.image.mv("./uploads"+image);
+
+    try{
+       let update ={
+        title:title,
+        description:des,
+        image:image
+       }
+       updateData("one_click","service",req.params.id,update);
+       res.send({message:"Service Updated Successfully",status:true});
+    }catch(e){
+        res.send({message:"Something Wrong",status:false});
+    }
+})
+app.get('/services',function(req,res){
+    try{ getAllData("one_click","service",res);
+}catch(e){
+}
+})
+app.get('/services/:id',function(req,res){
+    try{
+    getData("one_click","service",req.params.id,res);
+    }catch(e){
+
+    }
+})
+app.post('/services/:id/delete',fileUpload(),function(req,res){
+    var id = req.params.id;
+    deleteData("one_click","service",id);
+    res.send({message:"Service Deleted Successfully",status:true})
+})
+app.post('/user',fileUpload(),function(req,res){
+    let body = JSON.parse(req.body.data);
+    let username = body.username;
+    let email = body.email;
+    let password = body.password;
+    bcrypt.hash(password,10,(err,hash)=>{
+        try{
+            insertUser(username,email,hash,"one_click","users");
+            res.send({message:"User Created Successfully",status:true})
+         }catch(e){
+             res.send({message:"Something was wrong",status:false})
+         }
+    })
+
+    
+})
+app.get('/users',function(req,res){
+    try{ getAllData("one_click","users",res);
+}catch(e){
+}
+})
+app.post('/users/:id/delete',fileUpload(),function(req,res){
+    var id = req.params.id;
+    deleteData("one_click","users",id);
+    res.send({message:"User Deleted Successfully",status:true})
+})
+app.listen(8000,'127.0.0.1');
---
+++ models/DB.js
@@ -0,0 +1,113 @@
+const mongodb = require("mongodb");
+const mongodbClient = mongodb.MongoClient;
+const url ="mongodb://localhost:27017/";
+
+
+
+function createDB(dbName){
+    try{
+        mongodbClient.connect(url+dbName,function(err,db){
+            if(err){  throw err;}
+        })
+    }catch(e){
+
+    }
+}
+function createColletion(dbName,collName){
+    mongodbClient.connect(url,function(err,db){
+        if(err){throw err;}
+        var dbo = db.db(dbName);
+        dbo.createCollection(collName,function(err,res){
+            if(err){throw err;}
+            db.close();
+            console.log("Success")
+        })
+    })
+}
+function insertProduct(title,des,price,image,dbName,collName){
+    mongodbClient.connect(url,function(err,db){
+        if(err){throw err}
+        var dbo = db.db(dbName);
+        var data ={
+            title:title,
+            description:des,
+            image:image,
+            price:price
+        }
+        dbo.collection(collName).insertOne(data,function(err,res){
+            if(err){throw err};
+        })
+    })
+}
+function insertService(title,des,image,dbName,collName){
+ mongodbClient.connect(url,function(err,db){
+    if(err){throw err};
+    var dbo = db.db(dbName);
+    var data ={
+        title:title,
+        description:des,
+        image:image,
+    }
+    dbo.collection(collName).insertOne(data,function(err,response){
+        if(err){throw err} 
+    })
+ })
+}
+function insertUser(username,email,password,dbName,collName){
+    mongodbClient.connect(url,function(err,db){
+        if(err){throw err}
+        var dbo = db.db(dbName);
+        var data ={
+            username:username,
+            email:email,
+            password:password,
+        }
+        dbo.collection(collName).insertOne(data,function(err,res){
+            if(err){throw err};
+        })
+    })
+}
+function getAllData(dbName,collName,res){
+    mongodbClient.connect(url,function(err,db){
+        if(err){throw err;}
+          var dbo = db.db(dbName);
+          dbo.collection(collName).find({}).toArray(function(err,result){
+            if(err){throw err};
+            res.send(result);
+          })
+    })
+}
+function getData(dbName,collName,id,response){
+    mongodbClient.connect(url,function(err,db){
+        if(err){throw err};
+        var dbo = db.db(dbName);
+        dbo.collection(collName).find({_id:mongodb.ObjectId(id)}).toArray(function(err,result){
+            if(err){throw err};
+            response.send(result);
+        })
+
+    })
+}
+function updateData(dbName,collName,id,newUpdate){
+    mongodbClient.connect(url,function(err,db){
+        if(err){throw err};
+        var dbo = db.db(dbName);
+        var query = {_id:mongodb.ObjectId(id)};
+        var newData = {$set: newUpdate};
+
+        dbo.collection(collName).updateOne(query,newData,function(err,result){
+            if(err){throw err};
+        })
+    })
+}
+function deleteData(dbName,collName,id){
+    mongodbClient.connect(url,function(err,db){
+        if(err){throw err};
+        var dbo =db.db(dbName);
+        var query ={_id:mongodb.ObjectId(id)};
+        dbo.collection(collName).deleteOne(query,function(err,result){
+            if(err){throw err};
+        })
+    })
+}
+module.exports={createDB,createColletion,insertProduct,getAllData,getData,updateData,deleteData,insertService,insertUser};
